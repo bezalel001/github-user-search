3a7f8248f122f67506236d6b85b406d5b48211a3
bezalel001 <mickeybezalel@gmail.com>
2019-02-17 06:40:23 +0100

    Updates

---
+++ precache-manifest.7ceb7f47c85a34909370409b926f5998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aca5bba49fe672a4fa2a",
+    "url": "/github-user-search/static/js/runtime~main.aca5bba4.js"
+  },
+  {
+    "revision": "125731955d4eef56153f",
+    "url": "/github-user-search/static/js/main.12573195.chunk.js"
+  },
+  {
+    "revision": "ecf3d70b56c3db5240a7",
+    "url": "/github-user-search/static/js/2.ecf3d70b.chunk.js"
+  },
+  {
+    "revision": "125731955d4eef56153f",
+    "url": "/github-user-search/static/css/main.77ccbee9.chunk.css"
+  },
+  {
+    "revision": "ecf3d70b56c3db5240a7",
+    "url": "/github-user-search/static/css/2.4ce7e33c.chunk.css"
+  },
+  {
+    "revision": "bbed89a683e86f288803e89de55e9a00",
+    "url": "/github-user-search/index.html"
+  }
+];
